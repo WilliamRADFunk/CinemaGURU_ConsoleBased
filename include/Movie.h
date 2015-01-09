4b916db2cfd9f4c5b14238747cfd2755c89ba868
William RAD Funk <WilliamRADFunk@users.noreply.github.com>
2015-01-09 00:32:56 -0500

    *Multi-line comment added to the top of every game file stating the game's creator as William R.A.D. Funk and details the nature in which the game is permitted to play and distribute.

--- include/Movie.h
+++ include/Movie.h
@@ -1,3 +1,17 @@
+/*
+Created by William R.A.D. Funk
+Game content written by William R.A.D. Funk
+Design and code by William R.A.D. Funk
+Documentation by William R.A.D. Funk
+
+This game is the sole intellectual property of William R.A.D. Funk.
+It is free-to-play, and distribution of the game is acceptable as long
+as it remains free and that this multi-line comment is not altered in
+any way.
+
+Enjoy!
+*/
+
 #ifndef MOVIE_H
 #define MOVIE_H
 
